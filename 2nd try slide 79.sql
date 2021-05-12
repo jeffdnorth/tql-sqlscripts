f04cbcd3c5e47ba644fa5757d71074def56deb11
Jeff North <jnorth@tql.com>
2021-05-12 17:42:39 -0400

    message

---
+++ 2nd try slide 79.sql
@@ -0,0 +1,9 @@
+--2nd try slide 79
+
+SELECT * 
+	from instructor 
+	;
+select i.firstname , i.lastname , i.yearsexperience
+	from Instructor i 
+	order by	i.yearsexperience , i.firstname , i.lastname 
+	
