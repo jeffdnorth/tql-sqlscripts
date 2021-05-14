b4b46b5562683c4829c5e0e716c08b520f7436be
Jeff North <jnorth@tql.com>
2021-05-14 11:52:10 -0400

    message

---
+++ --create-database-create-table.sql
@@ -0,0 +1,25 @@
+--create-database-create-table
+--use master
+	--create database BcDb; 
+GO
+use Bootcamp
+create table Customers (
+	Id int primary key identity (1,1) ,
+	Code varchar (10) not null unique,
+	Name varchar (30) not null , 
+	Sales decimal (9,2) not null default 0 check (sales >=0),
+	Active bit not null default 1,
+	Created datetime not null default GETDATE() 
+	);
+GO
+SELECT *
+from Customers
+
+GO
+INSERT into Customers (Code, Name)
+	values	('TQ','Tota Quality Logistics');
+insert into Customers (Code, Name) 
+	values ('tql', 'should not work');
+insert into Customers (Code,Name,Sales)
+	values ('max', 'max tech', 200);
+GO
